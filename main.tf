61322214fc36c90affe5db0e7f2747814cbe5651
Roel Bruggink <roel@jaroel.nl>
2024-03-04 18:48:23 +0100

    Expose UZG on http://uzg.debian.orb.local/

--- main.tf
+++ main.tf
@@ -11,46 +11,62 @@ provider "docker" {
   host = "ssh://root@debian@orb"
 }
 
-# resource "docker_network" "proxy" {
-#   name = "proxy"
-# }
+resource "docker_network" "proxy" {
+  name = "proxy"
+}
 
-# #
-# # Traefik
-# #
-# resource "docker_image" "traefik" {
-#   name = "traefik:v3"
-# }
-# resource "docker_container" "traefik" {
-#   name    = "traefik"
-#   image   = docker_image.traefik.image_id
-#   restart = "unless-stopped"
+#
+# Traefik
+#
+resource "docker_image" "traefik" {
+  name = "traefik:v3.0"
+}
+resource "docker_container" "traefik" {
+  name    = "traefik"
+  image   = docker_image.traefik.image_id
+  restart = "unless-stopped"
 
-#   ports {
-#     internal = 80
-#     external = 80
-#   }
+  ports {
+    internal = 80
+    external = 80
+  }
 
-#   ports {
-#     internal = 443
-#     external = 443
-#   }
+  ports {
+    internal = 8080
+    external = 8080
+  }
 
-#   networks_advanced {
-#     name = docker_network.proxy.id
-#   }
-#   volumes {
-#     host_path      = "/var/run/docker.sock"
-#     container_path = "/tmp/docker.sock"
-#     read_only      = true
-#   }
-# }
+  networks_advanced {
+    name = docker_network.proxy.name
+  }
+  volumes {
+    host_path      = "/var/run/docker.sock"
+    container_path = "/var/run/docker.sock"
+    read_only      = true
+  }
+  command = [
+    "--api.insecure=true",
+    "--providers.docker=true",
+    "--providers.docker.exposedbydefault=false",
+    "--providers.docker.network=proxy",
+    "--entrypoints.web.address=:80",
+    # "--entrypoints.web.http.redirections.entrypoint.to=websecure",
+    # "--entrypoints.web.http.redirections.entrypoint.scheme=https",
+    # "--entrypoints.websecure.address=:443",
+    # "--entrypoints.websecure.asDefault=true",
+    # "--entrypoints.websecure.http.tls.certresolver=myresolver",
+    # "--certificatesresolvers.myresolver.acme.httpchallenge=true",
+    # "--certificatesresolvers.myresolver.acme.httpchallenge.entrypoint=web",
+    # "--certificatesresolvers.myresolver.acme.email=roel@jaroel.nl",
+    # "--certificatesresolvers.myresolver.acme.storage=/letsencrypt/acme.json",
+    # "--certificatesresolvers.myresolver.acme.caserver=https://acme-staging-v02.api.letsencrypt.org/directory",
+  ]
+}
 
 #
 # Dinxper FM Uitzending gemist
 #
 resource "docker_image" "dfmuzg" {
-  # dfmuzg:latest
   name         = "dfmuzg:latest"
   keep_locally = true
 }
@@ -58,13 +74,27 @@ resource "docker_container" "dfmuzg" {
   name    = "dfmuzg"
   image   = docker_image.dfmuzg.image_id
   restart = "unless-stopped"
+  command = ["python", "-m", "uvicorn", "server:app", "--host=0.0.0.0", "--port=8000"]
 
-  ports {
-    internal = 80
-    external = 8000
+  labels {
+    label = "traefik.enable"
+    value = "true"
+  }
+  labels {
+    label = "traefik.http.routers.dfmuzg.rule"
+    value = "Host(`uzg.debian.orb.local`)"
+  }
+  labels {
+    label = "traefik.http.routers.dfmuzg.entrypoints"
+    value = "web"
+  }
+  # labels {
+  #   label = "traefik.http.routers.dfmuzg.tls.certresolver"
+  #   value = "myresolver"
+  # }
+
+  networks_advanced {
+    name = docker_network.proxy.name
   }
 
-  # networks_advanced {
-  #   name = docker_network.proxy.id
-  # }
 }
--- push_containers.sh
+++ push_containers.sh
@@ -1,2 +1,2 @@
 docker save dfmsite:latest | bzip2 | pv | ssh root@debian@orb docker load
-docker save dfmproxy:latest | bzip2 | pv | ssh root@debian@orb docker load
+docker save dfmuzg:latest | bzip2 | pv | ssh root@debian@orb docker load
