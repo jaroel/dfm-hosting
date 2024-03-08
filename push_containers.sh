bc5fd7469b853f27b67dee153145456663a13b2a
Roel Bruggink <roel@jaroel.nl>
2024-03-08 18:11:39 +0100

    just use a cloud-config file

--- push_containers.sh
+++ push_containers.sh
@@ -1,2 +1,5 @@
 docker save dfmsite:latest | bzip2 | pv | ssh root@debian@orb docker load
 docker save dfmuzg:latest | bzip2 | pv | ssh root@debian@orb docker load
+
+
+docker save dfmuzg:latest | bzip2 | pv | ssh root@dfmhost.toffe.site docker load
